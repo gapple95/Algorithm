80ba59b4141247ff7c381f3cdb30b51f965ddb18
장현준 <50596733+gapple95@users.noreply.github.com>
2024-01-30 13:56:36 +0900

    [Silver V] Title: 재귀함수가 뭔가요?, Time: 328 ms, Memory: 18360 KB -BaekjoonHub

--- 백준/Silver/17478. 재귀함수가 뭔가요？/재귀함수가 뭔가요？.java
+++ 백준/Silver/17478. 재귀함수가 뭔가요？/재귀함수가 뭔가요？.java
@@ -25,8 +25,6 @@ ____라고 답변하였지.
  * 위의 예시
  * 
  */
-
-
 import java.util.Scanner;
 
 public class Main {
@@ -41,30 +39,30 @@ public class Main {
 		n = s.nextInt();
 		
 		System.out.println("어느 한 컴퓨터공학과 학생이 유명한 교수님을 찾아가 물었다.");
-		def(0);
+		def(n);
 	}
 	
 	public static void def(int i) {
-		for(int j=0; j<i; j++)
+		for(int j=n; j>i; j--)
 			System.out.print("____");
 		System.out.println("\"재귀함수가 뭔가요?\"");
-		if(n==i) { // 종료 조건
-			for(int j=0; j<i; j++)
+		if(i==0) { // 종료 조건
+			for(int j=n; j>i; j--)
 				System.out.print("____");
 			System.out.println("\"재귀함수는 자기 자신을 호출하는 함수라네\"");
 		} else {
-			for(int j=0; j<i; j++)
+			for(int j=n; j>i; j--)
 				System.out.print("____");
 			System.out.println("\"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.");
-			for(int j=0; j<i; j++)
+			for(int j=n; j>i; j--)
 				System.out.print("____");
 			System.out.println("마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.");
-			for(int j=0; j<i; j++)
+			for(int j=n; j>i; j--)
 				System.out.print("____");
 			System.out.println("그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어.\"");
-			def(i+1);
+			def(i-1);
 		}
-		for(int j=0; j<i; j++)
+		for(int j=n; j>i; j--)
 			System.out.print("____");
 		System.out.println("라고 답변하였지.");
 	}
