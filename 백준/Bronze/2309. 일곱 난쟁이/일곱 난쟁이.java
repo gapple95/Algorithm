c039505fa90cbf29959de070381a0a9cb39b7100
장현준 <50596733+gapple95@users.noreply.github.com>
2024-01-31 15:13:50 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 108 ms, Memory: 12916 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
@@ -1,55 +1,87 @@
 import java.util.Arrays;
 import java.util.Scanner;
 
+/*
+ * 백준 2309 일곱난쟁이
+ * 
+ * 일과를 마치고 돌아온 난쟁이가 일곱명이 아닌 아홉명이였던것!
+ * 
+ * 뛰어난 수학적 직관력을 가지고 있던 백성공주는,
+ * 다행스럽게도 일곱 난쟁이의 키의 합이 100이 됨을 기억해냈다.
+ * 
+ * 아홉 난쟁이의 키가 주어졌을 때, 백성공주를 도와 일곱 난쟁이를 찾는 프로그램을
+ * 작성하시오.
+ * 
+ * @입력
+ * 아홉 개의 줄에 걸쳐 난쟁이의 키가 주어진다.
+ * 전부 다해서 100을 넘지 않는 자연수이며 다 다르다.
+ * 가능한 정답이 여러가지인 경우 아무거나 출력한다.
+ * 
+ * @출력
+ * 일곱 난쟁이의 키를 오름차순으로 출력한다. 무조건 답이 있다.
+ * 
+ * 재귀로 모든 난쟁이의 키를 더하고, 100이면 출력
+ * 
+ */
+
 public class Main {
 
-	public static void main(String[] args){
+	static int[] arr;
+	static int[] dwap;
+	
+	static boolean[] isSelected;
+	static boolean success;
+	
+	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		int[] arr = new int[9];
-		
 		Scanner s = new Scanner(System.in);
 		
-		for(int i=0; i<9; i++) {
+		arr = new int[9];
+		for (int i = 0; i < arr.length; i++) {
 			arr[i] = s.nextInt();
 		}
 		
-		// 오름차순 정렬
-//		int tmp;
-//		for(int i=0; i<9; i++) {
-//			for(int j=i+1; j<9; j++) {
-//				if(arr[i] > arr[j]) {
-//					tmp = arr[i];
-//					arr[i] = arr[j];
-//					arr[j] = tmp;
-//				}
-//			}
+		dwap = new int[7];
+		isSelected = new boolean[9];
+		
+
+		df(0, 0, 0);
+		
+	}
+	
+//	static int sum() {
+//		int sum = 0;
+//		for (int i = 0; i < dwap.length; i++) {
+//			sum += dwap[i];
 //		}
-		// 오름차순 정렬
-		Arrays.sort(arr);
-		
-		int sum = Arrays.stream(arr).sum();
-		int diff = sum - 100;
-		int a = 0, b = 0;
-		
-//		System.out.println(diff);
-		
-		for(int i=0; i<9; i++) {
-			for(int j=0; j<9; j++) {
-//				System.out.println("i " + arr[i] + ", j " + arr[j]);
-				if(i==j) continue;
-				if (arr[i] + arr[j] == diff) {
-					a = i;
-					b = j;
-				}
-					
+//		return sum;
+//	}
+	
+	static void df(int idx, int sum, int start) {
+		if(success)
+			return;
+		if(idx == 7 && sum == 100) { // 기저조건
+			Arrays.sort(dwap);
+			for (int i : dwap) {
+				System.out.println(i);
 			}
+			success = true;
+			return;
+		} else if(idx == 7) {
+			return;
 		}
-		
-		for(int i=0; i<9; i++) {
-			if(i == a || i == b)
-				continue;
-			System.out.println(arr[i]);
+		else {
+			for (int i = start; i < arr.length; i++) {
+				if(isSelected[i])
+					continue;
+				dwap[idx] = arr[i];
+				isSelected[i] = true;
+				
+				df(idx + 1, sum + arr[i], i);
+				isSelected[i] = false;
+				
+			}
 		}
 	}
 
