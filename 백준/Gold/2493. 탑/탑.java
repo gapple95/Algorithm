02390ad5da8884500a78869c745214103992f94e
장현준 <50596733+gapple95@users.noreply.github.com>
2024-02-05 17:17:28 +0900

    [Gold V] Title: 탑, Time: 3140 ms, Memory: 180948 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.java
+++ 백준/Gold/2493. 탑/탑.java
@@ -5,51 +5,42 @@ import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class Main {
+    static int N;
+    static int[] arr;
 
-	static int N;
-	static int[] arr;
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		N = Integer.parseInt(br.readLine());
+        N = Integer.parseInt(br.readLine());
 
-		arr = new int[N];
+        arr = new int[N];
 
-		Stack<Integer> s1 = new Stack<>(); // 탑을 담는 스택
-		Stack<Integer> s2 = new Stack<>(); // 탑의 인덱스를 담는 스택
+        Stack<int[]> stack = new Stack<>(); // 높이와 인덱스 쌍을 저장하는 단일 스택 사용
 
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int get_num;
-		for (int i = 0; i < N; i++) {
-			get_num = Integer.parseInt(st.nextToken());
-			
-			// 스택 내부를 자신보다 큰 값이 있는지 확인하고 나보다 작으면 제거
-			for(int j=s1.size(); j>0; j--) {
-				if(s1.peek() > get_num)
-					break;
-				s1.pop();
-				s2.pop();
-			}
-			
-			// 모두 제거 후 스택이 다 비어진다면? 그게 가장 큰것이니 인덱스는 0 
-			// 안비어져 있다면 가장 큰것이 안쪽에 있다는 의미 => 안쪽의 인덱스를 정답 배열에 넣기
-			if (s1.isEmpty()) {
-				arr[i] = 0;
-			} else {
-				arr[i] = s2.peek();
-			}
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int get_num;
+        for (int i = 0; i < N; i++) {
+            get_num = Integer.parseInt(st.nextToken());
 
-			// 모든 계산후, 스택에 값을 넣고, 다음 인덱스도 넣는다.
-			s1.push(get_num);
-			s2.push(i+1);
-		}
+            // 더 큰 탑을 찾을 때까지 스택에서 요소를 팝합니다.
+            while (!stack.isEmpty() && stack.peek()[0] <= get_num) {
+                stack.pop();
+            }
 
-		for (int i = 0; i < N; i++) {
-            if(i==N-1)System.out.print(arr[i]);
-            else System.out.print(arr[i] + " ");
-		}
+            // 스택이 비어있으면 왼쪽에 더 높은 탑이 없습니다.
+            // 그렇지 않으면 왼쪽에 가장 높은 탑이 스택의 맨 위에 있습니다.
+            if (stack.isEmpty()) {
+                arr[i] = 0;
+            } else {
+                arr[i] = stack.peek()[1];
+            }
 
+            // 현재 탑의 높이와 인덱스를 스택에 푸시합니다.
+            stack.push(new int[]{get_num, i + 1});
+        }
 
-	}
-
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]+ " ");
+        }
+    }
 }
