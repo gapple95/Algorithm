313cf009dadec15b82adf6bb8daa32f5f0f0f4ec
장현준 <50596733+gapple95@users.noreply.github.com>
2025-01-13 01:57:19 +0900

    [Silver II] Title: 나무 자르기, Time: 444 ms, Memory: 167868 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.java
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.java
@@ -5,44 +5,54 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+	public static void main(String[] args) throws IOException {
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		st = new StringTokenizer(br.readLine());
+		
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		
+		st = new StringTokenizer(br.readLine());
+		
+		int[] trees = new int[N];
+		
+		int top_trees = Integer.MIN_VALUE;
+		for (int i = 0; i < trees.length; i++) {
+			trees[i] = Integer.parseInt(st.nextToken());
+			top_trees = Math.max(top_trees, trees[i]);
+		}
+		
+		int start, end, mid = 0;
+		
+		end = top_trees;
+		start = 0;
+		int result = 0;
+		while(start <= end) {
+			mid = start + (end - start) / 2;
+			
+			long sum = 0;
+			for (int i = 0; i < trees.length; i++) {
+				if(trees[i] - mid > 0)
+					sum += trees[i] - mid;
+			}
+			
+			if(sum >= M) {
+				result = mid;
+				start = mid + 1;
+			}
+			else {
+				end = mid - 1;
+			}
+		}
+		
+		sb.append(result);
+		System.out.println(sb);
+		
+		br.close();
+	}
 
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-
-        st = new StringTokenizer(br.readLine());
-        int[] trees = new int[N];
-        int top_trees = 0;
-
-        for (int i = 0; i < N; i++) {
-            trees[i] = Integer.parseInt(st.nextToken());
-            top_trees = Math.max(top_trees, trees[i]);
-        }
-
-        int start = 0;
-        int end = top_trees;
-        int result = 0;
-
-        while (start <= end) {
-            int mid = start + (end - start) / 2;
-
-            long sum = 0; // 나무 길이 합은 long으로 선언
-            for (int tree : trees) {
-                if (tree > mid) {
-                    sum += tree - mid;
-                }
-            }
-
-            if (sum >= M) { // 필요한 나무를 충족하면 더 높은 절단 높이 시도
-                result = mid;
-                start = mid + 1;
-            } else { // 필요한 나무를 충족하지 못하면 절단 높이를 낮춤
-                end = mid - 1;
-            }
-        }
-
-        System.out.println(result);
-    }
 }
