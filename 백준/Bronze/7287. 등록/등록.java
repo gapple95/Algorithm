8580f43ef278a25fafbfe20895596df450d89f86
장현준 <50596733+gapple95@users.noreply.github.com>
2025-01-21 01:54:06 +0900

    [Bronze V] Title: 등록, Time: 60 ms, Memory: 11372 KB -BaekjoonHub

---
+++ 백준/Bronze/7287. 등록/등록.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+        
+        sb.append("302").append("\n").append("wkdguswns100");
+        System.out.println(sb);
+    }
+}
