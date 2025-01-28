0ae26869d01bb99c90c7df1877912f136be59793
장현준 <50596733+gapple95@users.noreply.github.com>
2025-01-29 02:45:01 +0900

    [Gold IV] Title: 문자열 폭발, Time: 1028 ms, Memory: 88228 KB -BaekjoonHub

---
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.java
@@ -0,0 +1,57 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
+
+		st = new StringTokenizer(br.readLine());
+		String str = st.nextToken();
+
+		st = new StringTokenizer(br.readLine());
+		String tnt = st.nextToken();
+
+		Stack<Character> answer = new Stack<>();
+
+		for (int i = 0; i < str.length(); i++) {
+			answer.push(str.charAt(i));
+
+			// 끝에서 계속 폭발 문자열을 확인
+			if(answer.size() < tnt.length())
+				continue;
+			
+			// 끝부터 확인해보기
+			boolean flag = true;
+			for (int j = 0; j < tnt.length(); j++) {
+				if(answer.get(answer.size() - j - 1) != tnt.charAt(tnt.length() - j - 1))
+					flag = false;
+			}
+
+			if(flag) {
+				for (int j = 0; j < tnt.length(); j++) {
+					answer.pop();
+				}
+			}
+				
+		}
+
+		if(answer.isEmpty())
+			sb.append("FRULA");
+		else {
+			for (int i = 0; i < answer.size(); i++) {
+				sb.append(answer.get(i));
+			}
+		}
+
+		System.out.println(sb);
+
+		br.close();
+	}
+
+}
